79f588e03e09588b8933ad3b2873fac97d135a11
Peter-Xidian <mayowaodebode@gmail.com>
2021-07-13 18:33:51 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,65 @@
-/* ----- Display Menu when toggle is clicked-----*/
-const showMenu = (toggleId, navId) => {
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-
+    
+    // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show')
         })
     }
 }
+showMenu('nav-toggle','nav-menu')
 
-showMenu('nav-toggle', 'nav-menu')
-
-/* ----- Retract Menu after clickling on link-----*/
-const navLink = document.querySelectorAll('.nav__link'), navMenu = document.getElementById('nav-menu')
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/* ----- Scroll (Active link)-----*/
-const sections = document.querySelectorAll('section[id]')
 
-window.addEventListener('scroll', scrollActive)
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
     const scrollY = window.pageYOffset
 
-    sections.forEach(current=>{
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            DocumentType.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
-            DocumentType.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
-
-/* ----- Header Color Change-----*/
-window.onscroll = ()=>{
-    const nav = document.getElementById('header')
-    if (this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-}
-
-/*==== SCROLL REVEAL ANIMATION ====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 2000,
-    reset: true
-});
-
-sr.reveal(`.home__data, .home__img,
-            .sneaker, .collection__card,
-            .offer__container, .footer__box, .new__mens,
-            .new__sneaker-card`, {
-    interval: 200
-})
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
@@ -92,4 +90,16 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .sneaker__img, collection__img, .main__form`, {
+    interval: 200
 })
